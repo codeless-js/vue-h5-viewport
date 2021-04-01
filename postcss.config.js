60e6fc985e193e41625559a7fe61c7edb12c486c
tyuqing <tyuqing7@163.com>
2021-04-01 11:07:05 +0800

    chore: viewport方案配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
+    'postcss-px-to-viewport': {
+      viewportWidth: 375, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
+      selectorBlackList: ['.ignore', '.hairlines'], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+    },
   },
 };
